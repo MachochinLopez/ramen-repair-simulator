c13029ff643a69e62ff2dc91150ba2b0eb59540e
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2020-02-01 16:00:38 -0600

    Branch 3D

---
+++ Assets/Scripts/Repairable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repairable : Model
+{
+	//Se asigna el objeto desde la escena o su prefab.
+	[SerializeField]
+	private Model[] fragments;
+
+
+	public void Break()
+	{
+		//Spawnea al azar los pedazos del objeto.
+		//Les agrega un empuje pequeño en direcciones random.
+		//Saca particulas
+		//Destruye el objeto.
+	}
+}
---
+++ Assets/Scripts/Space.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space : MonoBehaviour
+{
+    [SerializeField]
+    private Collider col;
+    [SerializeField]
+    private GameObject fill;
+    [SerializeField]
+    private Vector3 relativePosition;
+    [SerializeField]
+    private float minimumSpeed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Si la maruchan entra en el trigger
+        float speed = other.GetComponent<Model>().getSpeed();
+
+        //Comparamos que la velocidad sea suficientemente rápida.
+        if(speed >= minimumSpeed)
+        {
+            //Instancia el fill del objeto.
+            Instantiate(fill, relativePosition, Quaternion.identity, gameObject.transform.parent);
+        }
+    }
+}
