002a6a12441da9646b23643a1d4ae29e1a01c5b1
Joel Mendoza González <60536967+wikijoel@users.noreply.github.com>
2020-02-01 18:11:14 -0600

    LevelManagement Scripts
    loool

---
+++ Scripts/Model.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+	Rigidbody rb;
+
+    void Start()
+    {
+		rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(getSpeed());
+    }
+
+    public float getSpeed()
+    {
+        float result = Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y) + Mathf.Abs(rb.velocity.z);
+        return result;
+    }
+
+}
---
+++ Scripts/Space.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fill;
+    [SerializeField]
+    private Vector3 relativePosition;
+    [SerializeField]
+    private float minimumSpeed;
+
+    private bool isFilled = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Si es una marushan...
+        if(other.gameObject.tag == "Ramen")
+        {
+            //Si la maruchan entra en el trigger
+            float speed = other.GetComponent<Model>().getSpeed();
+
+            //Comparamos que la velocidad sea suficientemente rápida.
+            if (speed >= minimumSpeed && !isFilled)
+            {
+                //Instancia el fill del objeto en la posición por defecto y como hijo del objeto a reparar.
+                Instantiate(fill, relativePosition, Quaternion.identity, gameObject.transform.parent);
+                isFilled = false;
+
+                return;
+            }
+        }
+    }
+}
